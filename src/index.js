5b5703c1032c25c163779f92419530809f5262aa
Jeremy Kahn <jeremy@jellyvision.com>
2018-07-10 14:46:36 -0500

    Handle more modification types

--- src/index.js
+++ src/index.js
@@ -12,20 +12,32 @@ export default class PropsEditor extends Component {
   }
 
   onEdit({ updated_src }) {
-    this.setState(Object.assign({}, updated_src));
+    // Start all properties off as undefined so that deleted props are cleared
+    // out when passed to setState
+    // https://stackoverflow.com/a/32884916
+    const newState = Object.keys(this.state).reduce((acc, key) => {
+      acc[key] = undefined;
+      return acc;
+    }, {});
+
+    this.setState(Object.assign(newState, updated_src));
   }
 
   render() {
     const Children = this.props.children.type;
+    const onEdit = this.onEdit.bind(this);
 
     return (
       <Fragment>
         <Children {...this.state} />
         <ReactJson
           displayDataTypes={false}
+          enableClipboard={false}
           name="props"
           src={this.state}
-          onEdit={this.onEdit.bind(this)}
+          onEdit={onEdit}
+          onAdd={onEdit}
+          onDelete={onEdit}
         />
       </Fragment>
     );
