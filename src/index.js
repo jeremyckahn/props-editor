f92ed6b2623b1cb51cc554be60989ed797353433
Jeremy Kahn <jeremy@jellyvision.com>
2018-07-10 11:35:01 -0500

    First pass implementation of PropEditor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export { App } from './app';
+import React, { Component, Fragment } from 'react';
+import { object } from 'prop-types';
+
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
+
+export default class PropEditor extends Component {
+  constructor(props) {
+    super(...arguments);
+
+    this.state = Object.assign({}, this.props.children.props, props);
+    delete this.state.children;
+  }
+
+  render() {
+    const Children = this.props.children.type;
+
+    return (
+      <Fragment>
+        <Children {...this.state} />
+        <Editor
+          value={this.state}
+          onChange={state => this.setState(Object.assign({}, state))}
+        />
+      </Fragment>
+    );
+  }
+}
+
+PropEditor.propTypes = {
+  children: object.isRequired,
+};
--- test/index.js
+++ test/index.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import { App } from '../src/index';
+import React, { Component } from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import assert from 'assert';
+import proxyquire from 'proxyquire';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+class JsonEditorStub extends Component {}
+
+const StubChild = () => {};
+
+const PropEditor = proxyquire.noCallThru().load('../src/index', {
+  'jsoneditor-react': { JsonEditor: () => JsonEditorStub },
+  'jsoneditor-react/es/editor.min.css': {
+    '@noCallThru': true,
+  },
+}).default;
+
 let component;
 
-describe('App', () => {
+describe('PropEditor', () => {
   beforeEach(() => {
-    component = shallow(<App />);
+    component = shallow(
+      <PropEditor>
+        <StubChild foo="bar" />
+      </PropEditor>
+    );
   });
 
-  describe('dom', () => {
-    it('renders content', () => {
-      assert.equal(component.find('div').length, 1);
+  describe('intercepting props', () => {
+    it('takes the props of the child as state', () => {
+      assert.deepEqual(component.state(), { foo: 'bar' });
     });
   });
 });
