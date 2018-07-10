76c81c81a9f8de7d8c907cf86264a7449ba54dd7
Jeremy Kahn <jeremy@jellyvision.com>
2018-07-10 14:46:54 -0500

    Make demo more practical

--- demo/index.js
+++ demo/index.js
@@ -1,31 +1,16 @@
-import React, { Component } from 'react';
-import ReactJson from 'react-json-view';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import PropsEditor from '../src/index.js';
 
-class DataVisualizer extends Component {
-  render() {
-    return (
-      <div>
-        <h1>
-          This is a read-only view of the <code>props</code> that are being
-          passed to the child:
-        </h1>
-        <ReactJson
-          enableClipboard={false}
-          name="child props (read-only)"
-          src={this.props}
-        />
-        <hr />
-      </div>
-    );
-  }
-}
-
 ReactDOM.render(
   <PropsEditor>
-    <DataVisualizer foo="bar" object={{ anArray: [1, 2, 3] }} />
+    <input
+      value="This can only be modified via props!"
+      type="text"
+      style={{ fontSize: '2em' }}
+      readOnly
+    />
   </PropsEditor>,
   document.getElementById('app')
 );
